4c0794f87422be7b87a6c70e3e7c6118766333ea
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-09-14 21:12:38 +0530

    Fix formatting and eligibility message in Vaccine.java

--- Vaccine.java
+++ Vaccine.java
@@ -3,7 +3,7 @@ class Vaccine
 {
      public static void main(String args[])
      {
-        System.out.println("Sana Tasneem Azimudin-2024503007");
+        System.out.println("Sana Tasneem Azimudin=2024503007");
         Scanner sc=new Scanner(System.in);
         int age,appno;
         System.out.println("Enter your age:");
